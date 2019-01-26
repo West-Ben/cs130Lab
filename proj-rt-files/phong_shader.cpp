f68118c03e88b6090196d6bb14149331f317b21c
West-Ben <benjamin.west.217@gmail.com>
2019-01-25 20:49:04 -0800

    phong3

--- proj-rt-files/phong_shader.cpp
+++ proj-rt-files/phong_shader.cpp
@@ -8,18 +8,21 @@ vec3 Phong_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
-    vec3 color = [0,0,0];
+    vec3 color;
+	color[0] = 0;
+	color[1] = 0;
+	color[2] = 0;
     //TODO; //determine the color
-	for (int i = 0; i < lights.size(); i++)
+	for (int i = 0; i < render_world.lights.size(); i++)
 	{
-		Ray lightRay(intersection_point,(lights[i].position - intersection_point).normalized());
-		Hit hit = Closest_Intersection(lightRay);
+		Ray lightRay(intersection_point,(render_world.lights[i].position - intersection_point).normalized());
+		Hit hit = render_world.Closest_Intersection(lightRay);
 		if (hit.object == NULL)
 		{
 			vec3 h = (ray.direction + lightRay.direction).normalized();
-			color += ( color_ambient * lights[i].Emitted_Light(lightRay.direction)) 
-			+ (lights[i].Emitted_Light(lightRay.direction) * color_diffuse * max(0,dot(normal,lightRay.direction)))
-			+ (lights[i].Emitted_Light(lightRay.direction) * color_specular * max(0,dot(h,ray.direction)));
+			color += ( color_ambient * render_world.lights[i].Emitted_Light(lightRay.direction)) 
+			+ (render_world.lights[i].Emitted_Light(lightRay.direction) * color_diffuse * max((double)0,(double)dot(normal,lightRay.direction)))
+			+ (render_world.lights[i].Emitted_Light(lightRay.direction) * color_specular * max((double)0,(double)dot(h,ray.direction)));
 		}
 	}
 	
