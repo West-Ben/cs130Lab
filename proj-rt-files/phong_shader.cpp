a215cde7aae0a0941effe0e5b05e2fc21d76f2dd
Benjamin West <benjamin.west.217@gmail.com>
2019-01-26 07:19:12 -0800

    phong_shader17

--- proj-rt-files/phong_shader.cpp
+++ proj-rt-files/phong_shader.cpp
@@ -15,11 +15,11 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 	{
 		Ray lightRay(intersection_point,(world.lights[i]->position - intersection_point).normalized());
 		Hit hit = world.Closest_Intersection(lightRay);
-		if (hit.object == NULL && hit.dist < 300)
+		if (hit.object == NULL && hit.dist < ((double)(1/lightRay.direction.magnitude_squared()) * world.lights[i]->brightness))
 		{
 			vec3 h = ((((double)-1) * ray.direction) + lightRay.direction).normalized();
-			//color += (world.lights[i]->Emitted_Light(lightRay.direction)) * ((color_diffuse * max((double)0,(double)dot(normal,lightRay.direction))) 
-			//+ (color_specular * pow(max((double)0,(double)dot(h,ray.direction)),specular_power)));
+			color += (world.lights[i]->Emitted_Light(lightRay.direction)) * ((color_diffuse * max((double)0,(double)dot(normal,lightRay.direction))) 
+			+ (color_specular * pow(max((double)0,(double)dot(h,ray.direction)),specular_power)));
 		}
 		else
 		{
