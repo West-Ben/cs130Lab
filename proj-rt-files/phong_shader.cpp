35f1b7f7d149a8e301e8644f2049812969e21c8c
West-Ben <benjamin.west.217@gmail.com>
2019-01-25 21:20:06 -0800

    phong11

--- proj-rt-files/phong_shader.cpp
+++ proj-rt-files/phong_shader.cpp
@@ -9,10 +9,7 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
     vec3 color;
-	color[0] = 0;
-	color[1] = 0;
-	color[2] = 0;
-	color += ( color_ambient * world.ambient_intensity);
+	color = ( color_ambient * world.ambient_intensity);
     //TODO; //determine the color
 	for (int i = 0; i < world.lights.size(); i++)
 	{
@@ -22,7 +19,11 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 		{
 			vec3 h = (ray.direction + lightRay.direction).normalized();
 			color += (world.lights[i]->Emitted_Light(lightRay.direction) * color_diffuse * max((double)0,(double)dot(normal,lightRay.direction)));
-			//+ (world.lights[i]->Emitted_Light(lightRay.direction) * color_specular * max((double)0,(double)dot(h,ray.direction)));
+			+ (specular_power * color_specular * max((double)0,(double)dot(h,ray.direction)));
+		}
+		else
+		{
+			world.background_shader->Shade_Surface(ray,intersection_point,normal.recursion_depth);
 		}
 	}
 	
