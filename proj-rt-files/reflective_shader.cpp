f27e8c5e473d2ca571ddec475c8d79a856307eec
Benjamin West <benjamin.west.217@gmail.com>
2019-02-02 18:59:20 -0800

    reflective shader

--- proj-rt-files/reflective_shader.cpp
+++ proj-rt-files/reflective_shader.cpp
@@ -6,7 +6,23 @@ vec3 Reflective_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
-    vec3 color;
-    TODO; // determine the color
-    return color;
+    vec3 color = shader->Shade_Surface(ray,intersection_point,normal,1);
+    //TODO;determine color
+    
+    Ray r;
+    r.endpoint = intersection_point;
+    r.direction = (ray.direction) - (2 * dot(ray.direction,normal) * normal);
+    
+    Hit hit = world.Closest_Intersection(r);
+    
+    if(hit.object == NULL || recursion_depth == 1)
+    {
+       return  color;
+    } 
+    else
+    {
+	vec3 point = r.Point(hit.dist);
+	
+    	return color += (reflectivity * Shade_Surface(r,point,hit.object->Normal(point,hit.part),(recursion_depth - 1)));
+    }
 }
